354cf62ae7912ca46a69b737982e02e119c08451
ahmtbrt07 <63432038+ahmtbrt07@users.noreply.github.com>
2025-04-13 03:38:46 +0300

    Update uniqSer.js
    error fix

--- uniqSer.js
+++ uniqSer.js
@@ -1,7 +1,7 @@
 import ax from 'axios';
 import fs from 'node:fs';
 
-const allList = process.argv[3]; // command line get domains  and subdomains list
+const allList = process.argv[2]; // command line get domains  and subdomains list
 
 fs.readFile(`${allList}`, 'utf8', (err, data) => {
   if (err) {
